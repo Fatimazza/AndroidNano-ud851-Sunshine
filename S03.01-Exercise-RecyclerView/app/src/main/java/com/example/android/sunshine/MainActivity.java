3bba7e848f34162f7dd1851fb49dd943abd7e4d4
Fatima Azzahro <fatima@paypro.id>
2017-09-10 20:30:19 +0700

    [Exercise3-1] Create object Adapter, Set it as Adapter for the RecyclerView

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -72,9 +72,11 @@ public class MainActivity extends AppCompatActivity {
         // COMPLETED (40) Use setHasFixedSize(true) on mRecyclerView to designate that all items in the list will have the same size
         mRecyclerView.setHasFixedSize(true);
 
-        // TODO (41) set mForecastAdapter equal to a new ForecastAdapter
+        // COMPLETED (41) set mForecastAdapter equal to a new ForecastAdapter
+        mForecastAdapter = new ForecastAdapter();
 
-        // TODO (42) Use mRecyclerView.setAdapter and pass in mForecastAdapter
+        // COMPLETED (42) Use mRecyclerView.setAdapter and pass in mForecastAdapter
+        mRecyclerView.setAdapter(mForecastAdapter);
 
         /*
          * The ProgressBar that will indicate to the user that we are loading data. It will be
