1c047e52410df2fabbe6d3dba96fd9eb2c36899e
Fatima Azzahro <fatima@paypro.id>
2017-09-10 21:18:47 +0700

    [Exercise3-2] Implement interface onClickHandler from Adapter in Main Activity, Override its onClick method

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -33,8 +33,8 @@ import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
 
-// TODO (8) Implement ForecastAdapterOnClickHandler from the MainActivity
-public class MainActivity extends AppCompatActivity {
+// COMPLETED (8) Implement ForecastAdapterOnClickHandler from the MainActivity
+public class MainActivity extends AppCompatActivity implements ForecastAdapter.ForecastAdapterOnClickHandler{
 
     private RecyclerView mRecyclerView;
     private ForecastAdapter mForecastAdapter;
@@ -107,8 +107,11 @@ public class MainActivity extends AppCompatActivity {
         new FetchWeatherTask().execute(location);
     }
 
-    // TODO (9) Override ForecastAdapterOnClickHandler's onClick method
     // TODO (10) Show a Toast when an item is clicked, displaying that item's weather data
+    // COMPLETED (9) Override ForecastAdapterOnClickHandler's onClick method
+    @Override
+    public void onClick(String weatherToday) {
+    }
 
     /**
      * This method will make the View for the weather data visible and
