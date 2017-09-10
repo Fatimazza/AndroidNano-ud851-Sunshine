dff2d699e6d4c5f44a7b8821f8cf6ef2ff4b270b
Fatima Azzahro <fatima@paypro.id>
2017-09-10 20:04:33 +0700

    [Exercise3-1] Override onBindViewHolder to display Weather data at the specific position

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -57,12 +57,13 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         return viewHolder;
     }
 
-    // TODO (27) Override onBindViewHolder
-    // TODO (28) Set the text of the TextView to the weather for this list item's position
+    // COMPLETED (27) Override onBindViewHolder
+    // COMPLETED (28) Set the text of the TextView to the weather for this list item's position
 
     @Override
     public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
-
+        String weatherForThisDay = mWeatherData[position];
+        holder.mWeatherTextView.setText(weatherForThisDay);
     }
 
     // TODO (29) Override getItemCount
