b2eb3d0563fec2a47be8233a4aef1685d255be20
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-08 18:31:31 +0700

    [Exercise 6-1] Settings Preference - Avoid spawning Main Activity class Instance when back button is pressed

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -2,16 +2,27 @@ package com.example.android.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
     // COMPLETED (1) Add new Activity called SettingsActivity using Android Studio wizard
     // Do step 2 in SettingsActivity
-    // TODO (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
+    // COMPLETED (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
