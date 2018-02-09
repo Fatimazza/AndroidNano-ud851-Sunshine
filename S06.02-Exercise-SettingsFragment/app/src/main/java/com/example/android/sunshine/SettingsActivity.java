982718ff85127b80c89ea8e08cb488fe44b11825
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-09 19:19:00 +0700

    [Exercise 6-2] Preference Fragment - Add method to set Preference Summary with pref Label associated with pref Value

--- S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -33,8 +33,6 @@ public class SettingsActivity extends AppCompatActivity {
         // Do steps 5 - 11 within SettingsFragment
         // TODO (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
 
-        // TODO (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
-
         // Do step 9 within onCreatePreference
         // TODO (9) Set the preference summary on each preference that isn't a CheckBoxPreference
 
--- S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.android.sunshine;
 
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
@@ -19,6 +21,31 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         /* Add 'general' preferences, defined in the XML file */
         addPreferencesFromResource(R.xml.pref_general);
 
+    }
+
+    // COMPLETED (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
+
+    public void setPreferenceSummary(Preference preference, Object value) {
+
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            /* For list preferences, look up the correct display value in */
+            /* the preference's 'entries' list (since they have separate labels/values). */
+
+            ListPreference listPreference = (ListPreference) preference;
+
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                //getting the Label which is associated with the Value
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
     }
 
 }
